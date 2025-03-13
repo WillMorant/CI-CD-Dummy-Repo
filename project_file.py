ddd361c2516d59512b5ea3298c86aa430eec7dd9
Will <morantwi@msu.edu>
2025-03-13 17:09:21 -0400

    first commit

---
+++ project_file.py
@@ -0,0 +1,16 @@
+# This file is a dummy project. It includes basic functions
+# that return static results that will be 'tested' in the test file.
+# The purpose of this is to practice automating a CI/CD Pipeline with
+# Jenkins
+
+def dummy_function_1():
+    return 1
+
+def dummy_function_2():
+    return 2
+
+def dummy_function_3():
+    return 3
+
+def dummy_function_4():
+    return 4
---
+++ project_test.py
@@ -0,0 +1,14 @@
+import pytest
+from project_file import *
+
+def test_dummy_function_1():
+    assert dummy_function_1() == 1
+
+def test_dummy_function_2():
+    assert dummy_function_2() == 2
+
+def test_dummy_function_3():
+    assert dummy_function_3() == 3
+
+def test_dummy_function_4():
+    assert dummy_function_4() == 4
